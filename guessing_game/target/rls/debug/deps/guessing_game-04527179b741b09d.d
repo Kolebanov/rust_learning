6972a0caf28d71e40e736785e9ed3e4ecde5fd3c
Victor Kolebanov <kolebanov@gmail.com>
2022-08-11 15:12:58 +0300

    add guessing_game folder, start 2nd project of rust

---
+++ guessing_game/target/rls/debug/deps/guessing_game-04527179b741b09d.d
@@ -0,0 +1,5 @@
+/home/victor/rust/rust_learning/guessing_game/target/rls/debug/deps/guessing_game-04527179b741b09d.rmeta: src/main.rs
+
+/home/victor/rust/rust_learning/guessing_game/target/rls/debug/deps/guessing_game-04527179b741b09d.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/rls/debug/deps/guessing_game-72767b5bddffa083.d
@@ -0,0 +1,5 @@
+/home/victor/rust/rust_learning/guessing_game/target/rls/debug/deps/guessing_game-72767b5bddffa083.rmeta: src/main.rs
+
+/home/victor/rust/rust_learning/guessing_game/target/rls/debug/deps/guessing_game-72767b5bddffa083.d: src/main.rs
+
+src/main.rs:
